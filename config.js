c1be624b6e63699fee1974a9a0aeb2d45fa90da1
John Alexander Ocampo Martinez <john.devsigner@gmail.com>
2025-06-26 15:02:15 -0500

    feat: ssl para conexion de db

--- config.js
+++ config.js
@@ -10,4 +10,5 @@ module.exports = {
     database: process.env.DB_NAME,
     quotes_port: process.env.QUOTES_PORT,
     sendgrid_key: process.env.SENDGRID_KEY,
+    ssl: process.env.DB_SSLMODE || 'disable',
 };
